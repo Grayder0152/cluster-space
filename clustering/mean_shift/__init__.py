9ea1ba6df093b849835d3bdc16b2f79b2431cab2
serhii_derevianko <serhii.derevianko_epam@kantar.com>
2024-02-21 00:35:14 +0200

    Added MeanShift clustering method;

---
+++ clustering/mean_shift/__init__.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+
+class MeanShift:
+    def __init__(self, bandwidth: float):
+        self.bandwidth = bandwidth
+
+    def clustering(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        points = dataframe.to_numpy()
+        n_points = len(points)
+        clusters = np.zeros(n_points, dtype=int)
+        centers = np.copy(points)
+
+        while True:
+            new_centers = []
+            for i in range(n_points):
+                center = centers[i]
+                distances = np.linalg.norm(points - center, axis=1)
+                in_bandwidth = points[distances < self.bandwidth]
+                new_center = np.mean(in_bandwidth, axis=0)
+                new_centers.append(new_center)
+
+            new_centers = np.array(new_centers)
+            if np.all(centers == new_centers):
+                break
+            centers = new_centers
+
+        for i, point in enumerate(points):
+            distances = np.linalg.norm(centers - point, axis=1)
+            clusters[i] = np.argmin(distances)
+
+        dataframe['cluster'] = clusters
+        return dataframe
+
+
+if __name__ == '__main__':
+    from extract_data import extract
+    from visualizer import Visualizer2D
+
+    df = extract('data_2.csv', delimiter=' ')
+    mean_shift = MeanShift(6.0)
+
+    clustered_df = mean_shift.clustering(df)
+
+    vis = Visualizer2D()
+    vis.plot(clustered_df)
+    vis.show()
